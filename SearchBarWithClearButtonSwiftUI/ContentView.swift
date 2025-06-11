f65ad8da66501c677a3c85811f3582abe96f6092
仲優樹 <yu-ki216m@outlook.jp>
2025-06-12 08:43:36 +0900

    Initial Commit

---
+++ SearchBarWithClearButtonSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchBarWithClearButtonSwiftUI
+//
+//  Created by 仲優樹 on 2025/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchBarWithClearButtonSwiftUI/SearchBarWithClearButtonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarWithClearButtonSwiftUIApp.swift
+//  SearchBarWithClearButtonSwiftUI
+//
+//  Created by 仲優樹 on 2025/06/12.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarWithClearButtonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
